8eb22a6a0e4e30889e4cea17e280589f6f69abe7
claashk <claashk@gmx.de>
2020-12-29 22:39:23 +0100

    * Moved asynchronous compress_const generator to asyncio sub-package

---
+++ vzclient/asyncio/compress.py
@@ -0,0 +1,26 @@
+from ..compress import Compressor
+
+
+async def compress_const(aiterable, max_gap=None):
+    """Compresses a time series by reducing number of nodes for const periods
+
+    Eliminates nodes where the measurement does not change. If ``max_gap`` is
+    specified, nodes are not deleted, if they
+
+    Arguments:
+        iterable (async interable): Async generator
+        max_gap (int): Maximum distance between neighbouring nodes.
+
+    Yield:
+        list: Compressed items of iterable with constant nodes removed
+    """
+    compressor = Compressor(max_gap=max_gap)
+    ait = aiterable.__aiter__()
+    try:
+        chunk = await ait.__anext__()
+        yield list(compressor.compress(compressor.iter(chunk)))
+    except StopAsyncIteration:
+        return
+    async for chunk in ait:
+        yield list(compressor.compress(chunk))
+    yield list(compressor.finalize())
