2617533dce5a940c08efdf6211ddb9d8366b6f85
claashk <claashk@gmx.de>
2020-12-31 11:53:08 +0100

    Moved timestamp calculation from mysql driver to a constants module

--- vzclient/asyncio/mysql_driver.py
+++ vzclient/asyncio/mysql_driver.py
@@ -1,6 +1,7 @@
 import aiomysql as sql
 from collections import namedtuple
-from datetime import datetime, timedelta
+from datetime import datetime
+from ..constants import timestamp
 
 Entity = namedtuple("Entity", ("id", "uuid", "type", "cls"))
 Entity.__doc__ = """Namedtuple for rows of the 'entities' table
@@ -15,32 +16,6 @@ Arguments:
 
 Measurement = namedtuple("Measurement", ("id", "channel_id", "timestamp", "value"))
 Property = namedtuple("Property", ("id", "entity_id", "key", "value"))
-
-EPOCH = datetime(year=1970, month=1, day=1)
-
-
-def time(t):
-    """Convert timestamp to datetime object
-
-    Arguments:
-        t (int): Timestamp [ms since EPOCH]
-
-    Return:
-        datetime.datetime: Datetime object representing the same time as `t`
-    """
-    return EPOCH + timedelta(milliseconds=t)
-
-
-def timestamp(t):
-    """Convert datetime object to timestamp
-
-    Arguments:
-        t (datetime.datetime): Datetime object
-
-    Return:
-        int: Timestamp [ms since EPOCH]
-    """
-    return int(1000. * (t - EPOCH).total_seconds() + 0.5)
 
 
 class MySqlDriver(object):
---
+++ vzclient/constants.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+
+EPOCH = datetime(year=1970, month=1, day=1)
+ 
+def time(t):
+    """Convert timestamp to datetime object
+
+    Arguments:
+        t (int): Timestamp [ms since EPOCH]
+
+    Return:
+        datetime.datetime: Datetime object representing the same time as `t`
+    """
+    return EPOCH + timedelta(milliseconds=t)
+
+
+def timestamp(t):
+    """Convert datetime object to timestamp
+
+    Arguments:
+        t (datetime.datetime): Datetime object
+
+    Return:
+        int: Timestamp [ms since EPOCH]
+    """
+    return int(1000. * (t - EPOCH).total_seconds() + 0.5)
+
